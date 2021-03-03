0eb7db13d32ca045bc9fb61225c055c905c82524
Nicole Sevillano <34521909+s3vi26@users.noreply.github.com>
2021-03-03 09:37:18 -0500

    ch ch changes

--- app.py
+++ app.py
@@ -4,4 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! My Name is Sevi"
+  return "Hello World! This is our first endpoint"
+
+@app.route("/upload")
+def uploading():
+  # read the uploaded files
+  # parse them to JSON
+  # throw errors for blank sheets
+  # write a fucntion to compare headers/comuns/rows
+  # maybe think about if a DB is needed here? 
+  return "This is the upload route. omg look at me gooooo"
